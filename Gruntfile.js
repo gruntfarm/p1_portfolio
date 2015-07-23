cbee209da9e53860178d788f5f6416127736fc52
Mladen Kovacevic <mladen.kovacevic@gmail.com>
2015-07-22 23:14:50 -0400

    compass working

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            name: 'small',
+            width: '480',
+            suffix: '_small',
+            quality: 50
+          },{
+            name: 'large',
+            width: '680',
+            suffix: '_large',
+            quality: 70
+          }]
+        },
+
+        /*
+        You don't need to change this part if you don't change
+        the directory structure.
+        */
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'img_src/',
+          dest: 'img/'
+        }]
+      }
+    },
+
+    clean: {
+      dev: {
+        src: ['img'],
+      },
+    },
+
+    mkdir: {
+      dev: {
+        options: {
+          create: ['img'],
+        },
+      },
+    },
+
+    copy: {
+      dev: {
+        files: [{
+          expand: true,
+          src: 'img_src/fixed/*.{gif,jpg,png}',
+          dest: 'img/'
+        }]
+      },
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-mkdir');
+  grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images']);
+
+};
